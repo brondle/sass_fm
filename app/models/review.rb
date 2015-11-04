bd78ea0f378de7d4baff4671c1f902f75eaf9851
Brent Bailey <baileyb@alumni.reed.edu>
2015-11-04 17:03:25 -0500

    functioning sign up and log in, adding current_session

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review<ActiveRecord::Base
 	belongs_to :album
+	belongs_to :user
 
 	validates :title, :body, :presence=> true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'sessions/new'
+
+  root 'users#new'
+  resources :users do
+    resources :reviews
+    resources :albums
+  end
+  resources :sessions
+
   resources :albums do
     resources :reviews
   end
